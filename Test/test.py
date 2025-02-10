7c08a64a4e534083d679b498d4d56cb28fc697e6
Alexander Kutschera <alexander.kutschera@rayngrowingsystems.com>
2025-02-10 14:14:48 +0100

    refactored cameraapp.py

--- Test/test.py
+++ Test/test.py
@@ -1,20 +1,16 @@
 from os import path
 import sys
 import time
-
-from PySide6.QtWidgets import QApplication
-from PySide6.QtCore import QCoreApplication, QStandardPaths
-from PySide6 import QtCore
-
-QApplication.setOrganizationName("RAYN")
-QApplication.setApplicationName("RAYN Vision System")
-
-app = QApplication([])
-app.setStyle( 'Fusion' )
-
+import stackprinter
 sys.path.append(path.join("..", "Application"))
 
+import cameraapp
 from MainWindow import MainWindow
+
+
+stackprinter.set_excepthook()
+rvs_app, splash = cameraapp.start_application(testing=True)
+cameraapp.start_logger(testing=True)
 
 script_folder = path.join("..", "Application", 'Scripts')
 mask_folder = path.join("..", "Application", 'Masks')
@@ -22,18 +18,22 @@ mask_folder = path.join("..", "Application", 'Masks')
 main_window = MainWindow(script_folder, mask_folder)
 
 main_window.resize(1200, 800)
-
 main_window.show()
 
 main_window.open_experiment_directly("wl_test.xp")   
-app.processEvents()
+rvs_app.processEvents()
 
 main_window.open_image_roi_dialog()
-app.processEvents()
+rvs_app.processEvents()
 
-while True:
-    app.processEvents()
-    time.sleep(1)
+main_window.open_image_mask_dialog()
+rvs_app.processEvents()
+
+try:
+    while True:
+        rvs_app.processEvents()
+        time.sleep(1)
     # How do we exit this? Check flag
-    
-# exit_code = app.exec()
+except KeyboardInterrupt():
+    print("stopping")
+    exit_code = rvs_app.exec()
