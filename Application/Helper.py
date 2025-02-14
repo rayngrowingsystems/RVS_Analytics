c4f232eb2e6e2507422c69a51eb2041034ff32b9
Anders Ekvall <anders.ekvall@etcconnect.com>
2025-02-14 12:10:26 +0100

    RAYNCAMANA-249: Preliminary support for "dropdownMultiSelect"
    Added checkboxes for multiple selections

--- Application/Helper.py
+++ Application/Helper.py
@@ -16,8 +16,10 @@
 
 from datetime import datetime
 
-from PySide6.QtWidgets import QDialog, QGridLayout, QCheckBox, QLabel, QPushButton, QMessageBox, QComboBox, QFrame
-from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QDialog, QGridLayout, QCheckBox, QLabel, QPushButton, QMessageBox, QComboBox, QFrame, QStyledItemDelegate
+from PySide6.QtCore import QTimer, QEvent
+from PySide6 import QtCore
+from PySide6.QtGui import QPalette, QFontMetrics, QStandardItem
 
 from DoubleSlider import DoubleSlider
 
@@ -148,6 +150,137 @@ def _parse_envi(headername):
 
     return header_dict, wavelength_dict
 
+class CheckableComboBox(QComboBox):
+
+    # Subclass Delegate to increase item height
+    class Delegate(QStyledItemDelegate):
+        def sizeHint(self, option, index):
+            size = super().sizeHint(option, index)
+            size.setHeight(20)
+            return size
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Make the combo editable to set a custom text, but readonly
+        self.setEditable(True)
+        self.lineEdit().setReadOnly(True)
+        # Make the lineedit the same color as QPushButton
+        palette = qApp.palette()
+        palette.setBrush(QPalette.Base, palette.button())
+        self.lineEdit().setPalette(palette)
+
+        # Use custom delegate
+        self.setItemDelegate(CheckableComboBox.Delegate())
+
+        # Update the text when an item is toggled
+        self.model().dataChanged.connect(self.updateText)
+        self.model().dataChanged.connect(self.signalChange)
+
+        # Hide and show popup when clicking the line edit
+        self.lineEdit().installEventFilter(self)
+        self.closeOnLineEditClick = False
+
+        # Prevent popup from closing when clicking on an item
+        self.view().viewport().installEventFilter(self)
+
+    def resizeEvent(self, event):
+        # Recompute text to elide as needed
+        self.updateText()
+        super().resizeEvent(event)
+
+    def eventFilter(self, object, event):
+
+        if object == self.lineEdit():
+            if event.type() == QEvent.MouseButtonRelease:
+                if self.closeOnLineEditClick:
+                    self.hidePopup()
+                else:
+                    self.showPopup()
+                return True
+            return False
+
+        if object == self.view().viewport():
+            if event.type() == QEvent.MouseButtonRelease:
+                index = self.view().indexAt(event.pos())
+                item = self.model().item(index.row())
+
+                if item.checkState() == QtCore.Qt.Checked:
+                    item.setCheckState(QtCore.Qt.Unchecked)
+                else:
+                    item.setCheckState(QtCore.Qt.Checked)
+
+                return True
+        return False
+
+    def showPopup(self):
+        super().showPopup()
+        # When the popup is displayed, a click on the lineedit should close it
+        self.closeOnLineEditClick = True
+
+    def hidePopup(self):
+        super().hidePopup()
+        # Used to prevent immediate reopening when clicking on the lineEdit
+        self.startTimer(100)
+        # Refresh the display text when closing
+        self.updateText()
+
+    def timerEvent(self, event):
+        # After timeout, kill timer, and reenable click on line edit
+        self.killTimer(event.timerId())
+        self.closeOnLineEditClick = False
+
+    def updateText(self):
+        texts = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == QtCore.Qt.Checked:
+                texts.append(self.model().item(i).text())
+        text = ", ".join(texts)
+
+        # Compute elided text (with "...")
+        metrics = QFontMetrics(self.lineEdit().font())
+        elidedText = metrics.elidedText(text, QtCore.Qt.ElideRight, self.lineEdit().width())
+        self.lineEdit().setText(elidedText)
+
+    def signalChange(self):
+        self.currentIndexChanged.emit(0)
+
+    def addItem(self, text, data=None):
+        item = QStandardItem()
+        item.setText(text)
+        if data is None:
+            item.setData(text)
+        else:
+            item.setData(data)
+        item.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsUserCheckable)
+        item.setData(QtCore.Qt.Unchecked, QtCore.Qt.CheckStateRole)
+        self.model().appendRow(item)
+
+    def addItems(self, texts, datalist=None):
+        for i, text in enumerate(texts):
+            try:
+                data = datalist[i]
+            except (TypeError, IndexError):
+                data = None
+            self.addItem(text, data)
+
+    def currentData(self):
+        # Return the list of selected items data
+        res = []
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).checkState() == QtCore.Qt.Checked:
+                res.append(self.model().item(i).data())
+        return res
+    
+    def setCurrentData(self, data):
+        for i in range(self.model().rowCount()):
+            if self.model().item(i).data() in data:
+                self.model().item(i).setCheckState(QtCore.Qt.Checked)
+            else:
+                self.model().item(i).setCheckState(QtCore.Qt.Unchecked)
+        
+        QTimer.singleShot(300, lambda:self.updateText())  # Delayed update to get size established
+    
 # Create grid of ui elements defined in a config file
 def get_ui_elements_from_config(options, settings, execute_on_change, dropdown_changed, slider_value_changed, wavelength_changed, script_for_dropdown_values):
     # Lists to hold various UI elements
@@ -312,7 +445,7 @@ def get_ui_elements_from_config(options, settings, execute_on_change, dropdown_c
             option_wavelength.currentIndexChanged.connect(execute_on_change)
 
         # Handle dropdown options
-        elif option["type"] == "dropdown":
+        elif option["type"] == "dropdown" or option["type"] == "dropdownMultiSelect":
             # Create a label for the dropdown option
             option_label = QLabel()
             option_label.setText(option["displayName"])
@@ -322,7 +455,11 @@ def get_ui_elements_from_config(options, settings, execute_on_change, dropdown_c
             row += 1
 
             # Create a dropdown menu
-            option_dropdown = QComboBox()
+            if option["type"] == "dropdown":
+                option_dropdown = QComboBox()
+            else:
+                option_dropdown = CheckableComboBox()
+
             if "hint" in option:
                  option_dropdown.setToolTip(option["hint"])
             grid.addWidget(option_dropdown, row, column)
@@ -345,12 +482,16 @@ def get_ui_elements_from_config(options, settings, execute_on_change, dropdown_c
             # Restore the last used index for the dropdown menu
             if option["name"] in settings:
                 value = settings[option["name"]]
-                index = option_dropdown.findData(value)
 
-                if index != -1:
-                    option_dropdown.setCurrentIndex(index)
+                if option["type"] == "dropdownMultiSelect":
+                    option_dropdown.setCurrentData(value)
                 else:
-                    option_dropdown.currentIndexChanged.emit(0)
+                    index = option_dropdown.findData(value)
+
+                    if index != -1:
+                        option_dropdown.setCurrentIndex(index)
+                    else:
+                        option_dropdown.currentIndexChanged.emit(0)
             else:
                 option_dropdown.currentIndexChanged.emit(0)
 
